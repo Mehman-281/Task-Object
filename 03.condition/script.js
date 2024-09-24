16147764abc424f5f86e0d2dc9bb87298404d1d2
Mehman-281 <mirzazade.281@gmail.com>
2024-09-24 10:44:50 +0400

    js keywords, back ticks, data types operators

---
+++ 03.condition/script.js
@@ -0,0 +1,34 @@
+let age = 32
+let email = `mehman@gmail.com`
+let password = `Salam123`
+let username = `mehman`
+let departament = `IT`
+
+if (age >= 31) {
+    console.log(`true`);
+} else {
+    console.log('false');
+
+}
+if (email === `mehman@gmail.com`)
+    console.log(`email true`);
+} else {
+    console.log();
+    
+
+}
+
+if (password >= 7) {
+    console.log(`true`);
+} else {
+    console.log(`false`);
+    
+}
+    
+
+
+
+
+
+
+
